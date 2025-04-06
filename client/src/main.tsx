4d25fcdd46eced5b34440d81c3b7071fdc3a4e8d
Odin Mühlenbein <odin.muehlenbein@gmail.com>
2025-04-06 23:28:24 +0200

    fixed unnecessary client router config change preventing /admin from showing up

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import { SessionProvider } from "./providers/SessionProvider";
 import { Admin } from "./admin/Admin";
 import "./index.css";
 
 // Use hash router for all routes to avoid 404s on direct URL access in production
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/admin/*",
     element: <Admin />,
