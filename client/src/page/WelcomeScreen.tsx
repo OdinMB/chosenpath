75ab22b872865e8c20e2dffd72a2410bd428a97e
Odin Mühlenbein <odin.muehlenbein@gmail.com>
2025-04-13 20:02:54 +0200

    welcome screen options order

--- client/src/page/WelcomeScreen.tsx
+++ client/src/page/WelcomeScreen.tsx
@@ -233,22 +233,6 @@ export function WelcomeScreen({
           Create Your Own Story
         </PrimaryButton>
 
-        {/* Template Browser */}
-        {onSelectTemplate && (
-          <>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-primary-100"></div>
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-primary-600">or</span>
-              </div>
-            </div>
-
-            <TemplateBrowser onSelectTemplate={handleSelectTemplate} />
-          </>
-        )}
-
         <div className="relative">
           <div className="absolute inset-0 flex items-center">
             <div className="w-full border-t border-primary-100"></div>
@@ -278,6 +262,17 @@ export function WelcomeScreen({
             </PrimaryButton>
           </div>
         </form>
+
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-primary-100"></div>
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="px-2 bg-white text-primary-600">or</span>
+          </div>
+        </div>
+
+        <TemplateBrowser onSelectTemplate={handleSelectTemplate} />
       </div>
 
       <footer className="mt-12 pt-4 border-t border-primary-100 text-xs text-primary-400">
