f730c6036e19636023f916be1e2f3dcfe70cbf6f
Odin Mühlenbein <odin.muehlenbein@gmail.com>
2025-04-28 14:38:12 +0200

    more heavy machinery against type issues for tests

--- server/src/__tests__/test.d.ts
+++ server/src/__tests__/test.d.ts
@@ -1,2 +1,17 @@
 import "mocha";
 import "chai";
+
+declare module "chai" {
+  export const expect: any;
+}
+
+declare function describe(name: string, fn: (this: Mocha.Suite) => void): void;
+declare function it(
+  name: string,
+  fn?: (this: Mocha.Context) => Promise<void> | void
+): void;
+
+interface Mocha {
+  Suite: any;
+  Context: any;
+}
